ddadd323b83f064f4a48f41298c2b830455f264c
Vwings <wings17@gmail.com>
2016-11-04 00:04:01 +0800

    support insert youku video

--- formats/video.js
+++ formats/video.js
@@ -9,10 +9,21 @@ const ATTRIBUTES = [
 
 class Video extends BlockEmbed {
   static create(value) {
+    if(value==undefined || value== null || value.trim().length == 0){
+      return ;
+    }
+    let srcValue = this.sanitize(value);
+    
+    if (srcValue == Link.SANITIZED_URL){
+      return ;
+    }
+    
     let node = super.create(value);
     node.setAttribute('frameborder', '0');
     node.setAttribute('allowfullscreen', true);
-    node.setAttribute('src', this.sanitize(value));
+    node.setAttribute('src', srcValue);
+    node.setAttribute('width' , 560);
+    node.setAttribute('height' , 268);
     return node;
   }
 
@@ -26,7 +37,7 @@ class Video extends BlockEmbed {
   }
 
   static sanitize(url) {
-    return Link.sanitize(url);
+    return sanitize(url);
   }
 
   static value(domNode) {
@@ -48,6 +59,27 @@ class Video extends BlockEmbed {
 Video.blotName = 'video';
 Video.className = 'ql-video';
 Video.tagName = 'IFRAME';
+Video.YOUKU_BASE = 'http://player.youku.com/embed/';
+Video.TUDOU_BASE = '';
+Video.BILIBILI_BASE = '';
+Video.YOUKUID_REGEXP = /(id_)(.*)(\.html)/;
+Video.TUDOUID_REGEXP = //;
+Video.BILIBILIID_REGEXP = //;
 
+
+function sanitize(url) {
+  return getYTBUrl(Link.sanitize(url));
+}
+
+function getYTBUrl(url) {
+  if (url.indexOf("v.youku.com") > -1){
+    return Video.YOUKU_BASE + YOUKUID_REGEXP.exec(url)[2];
+  }else if(url.indexOf("www.tudou.com") > -1){
+
+  }else if(url.indexOf("www.bilibili.com") > -1) {
+
+  }
+  return url;
+}
 
 export default Video;
